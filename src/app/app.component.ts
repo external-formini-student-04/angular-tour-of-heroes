54236b7deaefe570cd2b9d6ac134ea49618de072
Mohammed BOUCHAHDA <bouchahda.mohammed.pr@meteo.dz>
2025-02-20 12:57:26 +0100

    new app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'BOUCHAHDA Mohammed';
+  title = 'BOUCHAHDA Mohammed-20.02.2025';
 }
